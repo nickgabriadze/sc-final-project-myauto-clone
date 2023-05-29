702cf686a27e0a3e897c5b51b4d98a683f27ac7e
Nika Gabriadze <nickgabriadze@gmail.com>
2023-05-29 10:19:17 +0400

    formatted the code and removed unnecessary console log

--- src/hooks/useCategories.ts
+++ src/hooks/useCategories.ts
@@ -1,52 +1,42 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { Category } from "../components/search/searchInterfaces";
 import sortByCatTypes from "./sortByCatTypes";
 
-
 export const useCategories = (url: string) => {
-    const [catsData, setCatsData] = useState<{[key:string]: Category[]}>();
-    const [catsError, setCatsError] = useState<string>("OK")
-    const [catsLoading, setCatsLoading] = useState<boolean>(false);
+  const [catsData, setCatsData] = useState<{ [key: string]: Category[] }>();
+  const [catsError, setCatsError] = useState<string>("OK");
+  const [catsLoading, setCatsLoading] = useState<boolean>(false);
 
+  useEffect(() => {
+    const abortController = new AbortController();
 
-    useEffect(() => {
-       
-        const abortController = new AbortController();
+    const fetchData = async () => {
+      const req = await fetch(url);
+      const data = req.json();
 
+      return data;
+    };
 
-        const fetchData = async () => {
-            const req = await fetch(url);
-            const data = req.json();
-            
-            return data
-        }
+    try {
+      setCatsLoading(true);
+      const innerFunc = async () => {
+        const fetchedData = await fetchData();
+        setCatsData(sortByCatTypes(fetchedData.data));
+      };
 
-        try{
-            setCatsLoading(true)
-            const innerFunc = async () => {
-                const fetchedData = await fetchData();
-                console.log(fetchedData.data)
-                setCatsData(sortByCatTypes(fetchedData.data));
-                
-            }
+      innerFunc();
+    } catch (err) {
+      setCatsError("Couldn't fetch data");
+    } finally {
+      setTimeout(() => {
+        setCatsLoading(false);
+      }, 1000);
+    }
 
-            innerFunc();
-        }catch(err){
-            setCatsError("Couldn't fetch data")
-            
-        }finally{
-           setTimeout(() => {
-            setCatsLoading(false)
-           }, 1000)
-        }
+    return () => abortController.abort();
+  }, [url]);
 
-
-        return () => abortController.abort();
-
-        
-    }, [url])
-
-    return {catsData, catsLoading, catsError}
-}
+  return { catsData, catsLoading, catsError };
+};
 
 export default useCategories;
