99c66c4429755b6a7ba0410202fc6c23313e5abe
Gigi Jishkariani <gigijishkariani3108@gmail.com>
2023-06-01 17:30:21 +0400

    changed nothing, pulling updated commits

--- src/components/search/searchComponents/Selection/Selection.tsx
+++ src/components/search/searchComponents/Selection/Selection.tsx
@@ -1,12 +1,11 @@
-import carSearchStyling from "../../carSearch.module.css"
+import carSearchStyling from "../../carSearch.module.css";
 import useCategories from "../../../../hooks/useCategories";
 import { Category } from "../../searchInterfaces";
 import Manufacturers from "./Manufacturers";
 import { useAppSelector } from "../../../../features/hooks";
-
+import SelectTypes from "./SelectionType";
 
 function SelectionTypes() {
-
   const { catsData, catsError, catsLoading } = useCategories(
     "https://api2.myauto.ge/ka/cats/get"
   );
@@ -14,26 +13,12 @@ function SelectionTypes() {
   const { main_type } = useAppSelector((state) => state.searchReducer);
 
   return (
-    <div className={carSearchStyling["categories-wrapper"]}
-    >
-      <div className={carSearchStyling["deal-wrapper"]}
-     
-      >
-        <div className={carSearchStyling["deal-type"]}>
-          <h5>გარიგების ტიპი</h5>
-          <select name="Deal Type" className={carSearchStyling["deal-options"]}>
-            <option value="not chosen">გარიგების ტიპი</option>
-            <option value="იყიდება">იყიდება</option>
-            <option value="ქირავდება">ქირავდება</option>
-          </select>
-        </div>
-      </div>
+    <div className={carSearchStyling["categories-wrapper"]}>
 
-     <Manufacturers />
+      <SelectTypes />
+      <Manufacturers />
 
-      <div className={carSearchStyling["type-cat-wrapper"]}
-   
-      >
+      <div className={carSearchStyling["type-cat-wrapper"]}>
         <div className={carSearchStyling["cats-type"]}>
           <h5>კატეგორია</h5>
           <select name="Deal Type" className={carSearchStyling["cats-options"]}>
