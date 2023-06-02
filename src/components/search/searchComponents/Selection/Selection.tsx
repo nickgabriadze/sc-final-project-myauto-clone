4ef6b20b5377e87e8eb9de5b5289ddad48af9199
Gigi Jishkariani <gigijishkariani3108@gmail.com>
2023-06-02 15:04:04 +0400

    Made changes

--- src/components/search/searchComponents/Selection/Selection.tsx
+++ src/components/search/searchComponents/Selection/Selection.tsx
@@ -2,38 +2,17 @@ import carSearchStyling from "../../carSearch.module.css";
 import useCategories from "../../../../hooks/useCategories";
 import { Category } from "../../searchInterfaces";
 import Manufacturers from "./Manufacturers";
-import { useAppSelector } from "../../../../features/hooks";
-import SelectTypes from "./SelectionType";
 import Categories from "./Categories";
+import DealTypes from "./DealTypes";
 
 function SelectionTypes() {
-  const { catsData, catsError, catsLoading } = useCategories(
-    "https://api2.myauto.ge/ka/cats/get"
-  );
 
-  const { main_type } = useAppSelector((state) => state.searchReducer);
 
   return (
     <div className={carSearchStyling["categories-wrapper"]}>
 
-      <SelectTypes />
+      <DealTypes />
       <Manufacturers />
-
-      <div className={carSearchStyling["type-cat-wrapper"]}>
-        <div className={carSearchStyling["cats-type"]}>
-          <h5>კატეგორია</h5>
-          <select name="Deal Type" className={carSearchStyling["cats-options"]}>
-            <option value="loading">ყველა კატეგორია</option>
-            {catsData !== undefined
-              ? catsData[main_type].map((eachCategory: Category, index) => (
-                  <option key={index} value={eachCategory.category_id}>
-                    {eachCategory.seo_title.toLocaleUpperCase()}
-                  </option>
-                ))
-              : "loading..."}
-          </select>
-        </div>
-      </div>
       <Categories/>
 
     </div>
