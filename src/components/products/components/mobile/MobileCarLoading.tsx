d73457f81ab08b58560d58b4d10e078849be5ae7
Nika Gabriadze <nickgabriadze@gmail.com>
2023-06-12 16:34:13 +0400

    Mobile loading skeleton done

--- src/components/products/components/mobile/MobileCarLoading.tsx
+++ src/components/products/components/mobile/MobileCarLoading.tsx
@@ -1,8 +1,54 @@
 import mobileCarStyling from "./mobileCar.module.css";
 
-function MobileCarLoading(){
+function MobileCarLoading() {
+  return (
+    <div className={mobileCarStyling['m-skeleton']}>
+      <div className={mobileCarStyling["mobile-car-loading-skeleton"]}>
+        <div className={mobileCarStyling["m-skeleton-1"]}>
+          <div className={mobileCarStyling["vip-skeleton"]}></div>
+          <div className={mobileCarStyling["model-year-skeleton"]}></div>
+        </div>
 
-    return <div className={mobileCarStyling['mobile-car-loading-skeleton']}></div>
+        <div className={mobileCarStyling["m-skeleton-2"]}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+
+        <div className={mobileCarStyling["m-skeleton-3"]}></div>
+
+        <div className={mobileCarStyling["m-skeleton-4"]}>
+          <div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+
+          <div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+
+          <div></div>
+        </div>
+      </div>
+
+      <div className={mobileCarStyling["m-skeleton-5"]}>
+        <div>
+          <div className={mobileCarStyling['m-skeleton-icon']}></div>
+          <div className={mobileCarStyling['m-text-skeleton']}></div>
+          <div className={mobileCarStyling['m-text-skeleton']}></div>
+        </div>
+
+        <div>
+          <div className={mobileCarStyling['m-skeleton-icon']}></div>
+          <div className={mobileCarStyling['m-skeleton-icon']}></div>
+        </div>
+      </div>
+      </div>
+    
+  );
 }
 
 export default MobileCarLoading;
